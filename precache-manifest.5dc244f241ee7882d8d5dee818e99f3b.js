185c7b57880f62421327bc6ac01135b42ad71570
Jakub Jereczek <99jakubjereczek@gmail.com>
2020-09-05 13:54:48 +0200

    Updates

---
+++ precache-manifest.5dc244f241ee7882d8d5dee818e99f3b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91ef839363a7485ba76f076357ddf46f",
+    "url": "/react-redux-dayplanner/index.html"
+  },
+  {
+    "revision": "92097fa7d904558046ae",
+    "url": "/react-redux-dayplanner/static/css/main.5974a7fb.chunk.css"
+  },
+  {
+    "revision": "327404c01694ce331472",
+    "url": "/react-redux-dayplanner/static/js/2.de81a078.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-redux-dayplanner/static/js/2.de81a078.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92097fa7d904558046ae",
+    "url": "/react-redux-dayplanner/static/js/main.921b9a35.chunk.js"
+  },
+  {
+    "revision": "ff42cf1d5e7715148854",
+    "url": "/react-redux-dayplanner/static/js/runtime-main.bd37c4c5.js"
+  }
+]);
